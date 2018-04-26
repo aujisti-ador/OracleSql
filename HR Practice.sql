cffb50dc5bdd59d3dd62636e7c78063a6aa31669
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-04-27 02:02:38 +0600

    first commit

---
+++ HR Practice.sql
@@ -0,0 +1,6 @@
+select * from employees;
+select last_name from employees;
+select last_name,salary,salary+1000 from employees;
+select first_name||' '||last_name from employees;
+select 'The employee Id of ' ||first_name||' '||last_name||' '||'is '||employee_id as details,salary as wages from EMPLOYEES;
+select 'The employee Id of ' ||first_name||' '||last_name||' '||'is '||employee_id as details,salary from EMPLOYEES;
